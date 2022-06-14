fa4cfc605b47ced4327c6fafb0114caa4ea7485a
Nayru <101361108+nsorroza@users.noreply.github.com>
2022-06-14 07:04:21 -1200

    Clase #3

--- Program.cs
+++ Program.cs
@@ -1,34 +1,22 @@
 ﻿
+// Borra la pantalla
 Console.Clear();
 
-Console.WriteLine("Este programa calcula la superficie de un rectangulo");
-Console.WriteLine();
+// Pedimos al usuario que introduzca su nombre
+Console.WriteLine("Hola, Ingrese su nombre:");
 
-//decimal v = decimal.Parse("222");
+// Leemos el nombre del usuario
+string nombre = Console.ReadLine();
 
-//string baseRectanguloTexto = Console.ReadLine();
-//double baseRectangulo = double.Parse(baseRectanguloTexto);
+// Pedimos al usuario que introduzca su apellido
+Console.WriteLine("Ahora por favor ingrese su apellido:");
 
-Console.WriteLine("Ingrese la base del rectangulo");
-double baseRectangulo = double.Parse(Console.ReadLine());
+string apellido = Console.ReadLine();
 
-Console.WriteLine("Ingrese la altura del rectangulo");
-double alturaRectangulo = double.Parse(Console.ReadLine());
+// Mostramos el nombre
+Console.Write("Hola " + nombre);
+Console.WriteLine(", este es mi segundo programa.");
 
-double superficieRectangulo =
-    baseRectangulo * alturaRectangulo;
-
-Console.Write("La superficie del rectangulo es: ");
-Console.WriteLine(superficieRectangulo);
-
-bool esMayorADiez = superficieRectangulo > 10;
-
-if (esMayorADiez)
-{
-    // Mostrar mensaje solamente si es mayor a diez
-    Console.WriteLine("La superficie del rectangulo es mayor a 10");
-}
-
-//string cualquiercosa = "hola";
-
+Console.WriteLine("Pulse una tecla para finalizar.");
 Console.ReadKey();
+
