e333ea7e86da98587275a307e539e7895cd6c112
Nayru <101361108+nsorroza@users.noreply.github.com>
2022-06-14 07:31:06 -1200

    Clase #10

--- Program.cs
+++ Program.cs
@@ -1,56 +1,136 @@
-﻿//Se dispone de un array de n números aleatorios (Usar la clase Random). 
-//Diseñar un programa que permita insertar un valor x en el lugar k-ésimo del array
+﻿/*
+1) Arrays
+2) Listas
+3) Colas
+4) Pilas
+5) Diccionarios
+etc...
+ */
 
-Console.WriteLine("Ingrese el tamaño deseado para el listado:");
-var dato = Console.ReadLine();
-int n = int.Parse(dato);
+////Inicialización de Array
+//var miArray = new int[10];
 
-int[] miVector = new int[n];
+////Recorro un array
+//for (int i = 0; i < miArray.Length; i++)
+//{
+//    Console.WriteLine(miArray[i]);
+//}
 
-Random aleatorio = new Random();
+//foreach(var item in miArray)
+//{
+//    Console.WriteLine(item);
+//}
 
-for (int i = 0; i < miVector.Length; i++)
+//var personas = new List<string>();
+
+//personas.Add("Ezequiel");
+//personas.Add("Joaquín");
+//personas.Add("Eugenio");
+
+//foreach(var persona in personas)
+//{
+//    Console.WriteLine(persona);
+//}
+
+/*
+ * Escribir un programa que permita al usuario confeccionar 
+ * una lista de nombres. Se debe ofrecer las siguientes opciones: 
+ * agregar un elemento (al final o en una posición determinada), 
+ * quitar un elemento, mostrar en qué posición se encuentra 
+ * un determinado elemento, invertir la lista.
+ */
+
+var listaNombres = new List<string>();
+
+Console.WriteLine("Bienvenido a Lista.");
+
+string entradaUsuario;
+do
 {
-    miVector[i] = aleatorio.Next(100);
-}
+    Console.WriteLine("Seleccione una de las siguientes opciones:");
+    Console.WriteLine("1-Agregar un elemento");
+    Console.WriteLine("2-Quitar un elemento");
+    Console.WriteLine("3-Ubicar un elemento");
+    Console.WriteLine("4-Invertir la lista");
+    Console.WriteLine("5-Ver el contenido de la lista");
+    Console.WriteLine("0-Salir");
 
-var contador = 0;
-foreach (var i in miVector)
-{
-    contador++;
-    Console.WriteLine(contador + ": " + i + " ");
-}
+    entradaUsuario = Console.ReadLine();
 
-Console.WriteLine();
-Console.WriteLine("Ingrese posición a modificar:");
-var k = int.Parse(Console.ReadLine());
-
-if (k > miVector.Length || k < 0)
-{
-    Console.WriteLine("Indice incorrecto");
-}
-else
-{
-    Console.WriteLine("Ingrese nuevo valor:");
-
-    while (true) // Bucle infinito
+    switch (entradaUsuario)
     {
-        var nuevoValor = int.Parse(Console.ReadLine());
-        if(nuevoValor>0 && nuevoValor < 100)
-        {
-            miVector[k-1] = nuevoValor;
-            break; //Para salir del bucle infinito
-        }
-        else
-        {
-            Console.WriteLine("Recuerde que el valor debe ser positivo menor que 100");
-        }
-    }
+        case "1":
+            //Agregar un elemento
+            Console.WriteLine("Ingrese el elemento:");
+            var nuevoElemento = Console.ReadLine();
 
-    contador = 0;
-    foreach (var i in miVector)
-    {
-        contador++;
-        Console.WriteLine(contador + ": " + i + " ");
+            Console.WriteLine("Dónde desea agregar el elemento?");
+            Console.WriteLine("1-Al final");
+            Console.WriteLine("2-En una posición determinada");
+
+            var opcionParaIngresar = Console.ReadLine();
+            switch (opcionParaIngresar)
+            {
+                case "1":
+                    //Al final
+                    listaNombres.Add(nuevoElemento);
+                    break;
+
+                case "2":
+                    //Insertar un elemento en una posición
+                    Console.WriteLine("Ingrese posición:");
+                    var posicionAIngresar = int.Parse(Console.ReadLine());
+
+                    listaNombres.Insert(posicionAIngresar, nuevoElemento);
+
+                    break;
+            }
+            break;
+
+        case "2":
+            //Quitar elemento
+            Console.WriteLine("Ingrese el elemento que desea quitar:");
+            var elementoARemover = Console.ReadLine();
+            listaNombres.Remove(elementoARemover);
+
+            break;
+
+        case "3":
+            //Ubicar un elemento
+            Console.WriteLine("Ingrese el elemento que desea ubicar:");
+            var elementoAUbicar = Console.ReadLine();
+            var ubicacion = listaNombres.IndexOf(elementoAUbicar);
+            if (ubicacion > -1)
+            {
+                Console.WriteLine("El elemento se encuentra en la posición " + ubicacion);
+            }
+            else
+            {
+                Console.WriteLine("Ese elemento no existe");
+            }
+
+            break;
+
+        case "4":
+            //Invertir la lista
+            listaNombres.Reverse();
+            Console.WriteLine("La lista se ha invertido.");
+            break;
+
+        case "5":
+            //Ver lista
+            if (listaNombres.Count == 0)
+            {
+                Console.WriteLine("La lista está vacía.");
+            }
+            else
+            {
+                foreach (var item in listaNombres)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            break;
     }
 }
+while (entradaUsuario != "0");
