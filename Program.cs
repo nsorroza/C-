840391221bfdb8921da4b18f64f92ad9dc88b699
Nayru <101361108+nsorroza@users.noreply.github.com>
2022-06-14 07:10:17 -1200

    Ejercicios Array - Clase #9

--- Program.cs
+++ Program.cs
@@ -1,127 +1,56 @@
-﻿var bingo = new int[3, 9];
+﻿//Se dispone de un array de n números aleatorios (Usar la clase Random). 
+//Diseñar un programa que permita insertar un valor x en el lugar k-ésimo del array
 
-var columnas1 = 3;
-var columnas2 = 6;
-int[] filas = new int[3];
-filas[0] = 0;
-filas[1] = 0;
-filas[2] = 0;
+Console.WriteLine("Ingrese el tamaño deseado para el listado:");
+var dato = Console.ReadLine();
+int n = int.Parse(dato);
 
-//for (int i = 0; i < 9; i++)
-//{
-//    var min = 0;
-//    var max = 0;
-//    switch (i)
-//    {
-//        case 0:
-//            min = 1;
-//            max = 10;
-//            break;
-//        case 8:
-//            min = 80;
-//            max = 91;
-//            break;
-//        default:
-//            min = i * 10;
-//            max = i * 20;
-//            break;
-//    }
+int[] miVector = new int[n];
 
-//    var columna = new Random().Next(1, 3);
-//    if (columna == 1 && columnas1 > 0)
-//    {
-//        columnas1--;
-//    }
-//    else
-//    {
-//        columnas2--;
-//    }
-//}
+Random aleatorio = new Random();
 
-
-
-for (int i = 0; i < 9; i++)
+for (int i = 0; i < miVector.Length; i++)
 {
-    var min = 0;
-    var max = 0;
-
-    int maxValue = filas.Max();
-    int maxIndex = filas.ToList().IndexOf(maxValue);
-
-    switch (i)
-    {
-        case 0:
-            min = 1;
-            max = 10;
-            break;
-        case 8:
-            min = 80;
-            max = 91;
-            break;
-        default:
-            min = i * 10;
-            max = i * 20;
-            break;
-    }
-
-    var columna = new Random().Next(1, 3);
-    if (columna == 1 && columnas1 > 0)
-    {
-
-        columnas1--;
-
-        var fila = new Random().Next(0, 3);
-
-        while (filas[fila] == 5 || fila == maxIndex)
-        {
-            fila = new Random().Next(0, 3);
-        }
-        filas[fila]++;
-
-        var valor = new Random().Next(min, max);
-
-        bingo[fila, i] = valor;
-    }
-    else
-    {
-        if (i == 8)
-        {
-            Console.WriteLine();
-        }
-        columnas2--;
-
-        var fila = new Random().Next(0, 3);
-        while (filas[fila] == 5 || fila == maxIndex)
-        {
-            fila = new Random().Next(0, 3);
-        }
-        filas[fila]++;
-
-
-        var valor = new Random().Next(min, max);
-
-        bingo[fila, i] = valor;
-
-        var fila2 = new Random().Next(0, 3);
-        while (filas[fila2] == 5 || fila2 == maxIndex)
-        {
-            fila2 = new Random().Next(0, 3);
-        }
-        filas[fila2]++;
-
-
-        var valor2 = new Random().Next(min, max);
-
-        bingo[fila2, i] = valor2;
-
-    }
+    miVector[i] = aleatorio.Next(100);
 }
 
-for (int i = 0; i < 3; i++)
+var contador = 0;
+foreach (var i in miVector)
 {
-    for (int x = 0; x < 9; x++)
+    contador++;
+    Console.WriteLine(contador + ": " + i + " ");
+}
+
+Console.WriteLine();
+Console.WriteLine("Ingrese posición a modificar:");
+var k = int.Parse(Console.ReadLine());
+
+if (k > miVector.Length || k < 0)
+{
+    Console.WriteLine("Indice incorrecto");
+}
+else
+{
+    Console.WriteLine("Ingrese nuevo valor:");
+
+    while (true) // Bucle infinito
     {
-        Console.WriteLine($"{i} {x} = " + bingo[i, x]);
+        var nuevoValor = int.Parse(Console.ReadLine());
+        if(nuevoValor>0 && nuevoValor < 100)
+        {
+            miVector[k-1] = nuevoValor;
+            break; //Para salir del bucle infinito
+        }
+        else
+        {
+            Console.WriteLine("Recuerde que el valor debe ser positivo menor que 100");
+        }
     }
-    Console.WriteLine();
+
+    contador = 0;
+    foreach (var i in miVector)
+    {
+        contador++;
+        Console.WriteLine(contador + ": " + i + " ");
+    }
 }
