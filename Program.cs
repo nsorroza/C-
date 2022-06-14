9d7cac0e9043be7232349769795db96cabd0de96
Nayru <101361108+nsorroza@users.noreply.github.com>
2022-06-14 07:34:41 -1200

    Constructores - Clase #13

--- Program.cs
+++ Program.cs
@@ -1,28 +1,11 @@
-﻿using Alumnitos;
+﻿using Constructores.Modelo;
 
-var alumno1 = new Alumno();
+var persona1 = new Persona("Ezequiel Etchecoin", "12312", 30, true);
 
-alumno1.Apellido = "Mateos";
-alumno1.Nombre = "Joaquin";
-alumno1.Legajo = 1;
-alumno1.DNI = "30.560.420";
-alumno1.FechaNacimiento = new DateTime(1986, 11, 4);
-alumno1.Calificaciones = new List<int>();
+Console.WriteLine(persona1.ObtenerSaludo());
+Console.WriteLine(persona1.ObtenerSaludo("Carlos"));
 
+var persona2 = new Persona { NombreCompleto = "Carlos Moscoso" };
 
-
-var alumno2 = new Alumno()
-{
-    Apellido = "Etchecoin",
-    Nombre = "Ezequiel",
-    Legajo = 2,
-    DNI = "42.560.220",
-    FechaNacimiento = new DateTime(2000,6,2)
-};
-
-
-alumno1.CargarCalificacion(5, new DateTime(2022,6,1));
-alumno1.CargarCalificacion(9, new DateTime(2022,4,10));
-alumno1.CargarCalificacion(8);
-
-Console.WriteLine(alumno1.ObtenerPromedio());
+//Esta clase también se incorpora desde el namespace Constructores.Modelo
+var miOcupacion = new Ocupacion();
